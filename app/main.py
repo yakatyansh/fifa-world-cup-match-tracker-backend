84e43dba1e05323c01e482b5a2b849f29b23d348
yakatyansh <ykpetrolhead08@outlook.com>
2026-06-11 13:12:28 +0530

    fix

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://fifa-world-cup-match-tracker-fronte.vercel.app/"],
+    allow_origins=["https://fifa-world-cup-match-tracker-fronte.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
