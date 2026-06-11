506495ed3475b36b052bb630eb31d2b73d9635b3
yakatyansh <ykpetrolhead08@outlook.com>
2026-06-11 21:47:06 +0530

    replace local data with matchesapi

--- app/routes/matches.py
+++ app/routes/matches.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-import json
+from app.services.worldcup import get_games
 
 router = APIRouter()
 
@@ -7,12 +7,34 @@ router = APIRouter()
 @router.get("/matches")
 def get_matches(teams: str | None = None):
 
-    with open("data/matches.json", "r", encoding="utf-8") as f:
-        matches = json.load(f)
+    data = get_games()
+
+    matches = []
+
+    for game in data["games"]:
+        matches.append(
+            {
+                "id": game["id"],
+                "home_team": game["home_team_name_en"],
+                "away_team": game["away_team_name_en"],
+                "group": game["group"],
+                "matchday": game["matchday"],
+                "date": game["local_date"],
+                "stadium_id": game["stadium_id"],
+                "finished": game["finished"],
+                "time_elapsed": game["time_elapsed"],
+                "type": game["type"],
+                "home_score": game["home_score"],
+                "away_score": game["away_score"]
+            }
+        )
+
     if teams:
 
-        team_list = [team.strip() for team in teams.split(",")]
-
+        team_list = [
+            team.strip()
+            for team in teams.split(",")
+        ]
 
         matches = [
             match
@@ -21,7 +43,6 @@ def get_matches(teams: str | None = None):
                 match["home_team"] in team_list
                 or match["away_team"] in team_list
             )
-            
         ]
 
     return matches
---
+++ app/services/worldcup.py
@@ -0,0 +1,11 @@
+import requests
+
+BASE_URL = "https://worldcup26.ir"
+
+def get_teams():
+    response = requests.get(f"{BASE_URL}/get/teams")
+    return response.json()
+
+def get_games():
+    response = requests.get(f"{BASE_URL}/get/games")
+    return response.json()
